ee1cda0a8b4b268b185a85769b90ac35a16eb5a2
kevinB <kevinB@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-07 20:44:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/role-scoper/tags/1.0.2@116234 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ db-setup_rs.php
@@ -0,0 +1,206 @@
+<?php
+if( basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME']) )
+	die();
+
+global $wpdb;
+require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+
+
+function scoper_db_setup($last_db_ver) {
+	scoper_update_schema($last_db_ver);
+	
+	global $scoper_db_setup_done;
+	$scoper_db_setup_done = 1;
+}
+
+function scoper_update_schema($last_db_ver) {
+	global $wpdb;
+	
+	/*--- Groups table def 
+		(complicated because (a) we support usage of pre-existing group table from other app
+		 					 (b) existing group table may include a subset of our required columns
+		 					 (c) existing group table may require authenticated/unauthenticated default group flag to be stored in two different columns 
+	*/
+	
+	//first define column(s) to create for default groups
+	$cols = array();
+	$cols[$wpdb->groups_id_col] = "bigint(20) NOT NULL auto_increment";
+	$cols[$wpdb->groups_name_col] = "text NOT NULL default ''";
+	$cols[$wpdb->groups_descript_col] = "text NOT NULL default ''";
+	$cols[$wpdb->groups_homepage_col] = "varchar(128) NOT NULL default ''";
+	$cols[$wpdb->groups_meta_id_col] = "varchar(32) NOT NULL default ''";
+	
+	if($tables = $wpdb->get_col('SHOW TABLES;'))
+		foreach($tables as $table)
+			if ($table == $wpdb->groups_rs)
+				break;	
+				
+	if ( $table != $wpdb->groups_rs ) { //group table doesn't already exist
+		 $query = "CREATE TABLE IF NOT EXISTS " . $wpdb->groups_rs . ' (';
+		 
+		 foreach ($cols as $colname => $typedef)
+		 	$query .= $colname . ' ' . $typedef . ',';
+
+		$query .= " PRIMARY KEY ($wpdb->groups_id_col),"
+				. " KEY meta_id ($wpdb->groups_meta_id_col, $wpdb->groups_id_col) );";
+	
+		$wpdb->query($query);
+		
+	} else {
+		//specified group table exists already, so do not alter any existing columns
+		// (we're not fussy about data types since the joins to these tables are infrequent and/or buffered, 
+		// but other app(s) might be fussy).
+		
+		$tablefields = $wpdb->get_col("DESC $wpdb->groups_rs", 0);
+		
+		if ( $add_cols = array_diff_key( $cols, array_flip($tablefields) ) ) {
+			foreach ( $add_cols as $requiredcol_name => $requiredcol_typedef ) {
+				if ( $requiredcol_name == $wpdb->groups_id_col ) // don't try to add id column
+					continue;
+
+				$wpdb->query("ALTER TABLE $wpdb->groups_rs ADD COLUMN $requiredcol_name $requiredcol_typedef");	
+				
+				if ( $wpdb->groups_meta_id_col == $requiredcol_name )
+					$wpdb->query( "CREATE INDEX meta_id ON $wpdb->groups_rs ($wpdb->groups_meta_id_col, $wpdb->groups_id_col)" );
+			}
+		}
+		
+		if ( ! version_compare( $last_db_ver, '1.0.2', '>=') ) {
+			// DB version < 1.0.2 used varchar columns, which don't support unicode
+			$wpdb->query("ALTER TABLE $wpdb->groups_rs MODIFY COLUMN $wpdb->groups_name_col text NOT NULL default ''");
+			$wpdb->query("ALTER TABLE $wpdb->groups_rs MODIFY COLUMN $wpdb->groups_descript_col text NOT NULL default ''");	
+		}
+	}
+
+	
+	// User2Group table def (use existing table from other app if so defined in grp-config.php)
+	$cols = array();
+	$cols[$wpdb->user2group_gid_col] = "bigint(20) unsigned NOT NULL default '0'";
+	$cols[$wpdb->user2group_uid_col] = "bigint(20) unsigned NOT NULL default '0'";
+	$cols[$wpdb->user2group_assigner_id_col] = "bigint(20) unsigned NOT NULL default '0'";
+				 
+	if($tables = $wpdb->get_col('SHOW TABLES;'))
+		foreach($tables as $table)
+			if ($table == $wpdb->user2group_rs)
+				break;	
+	
+	if ( $table != $wpdb->user2group_rs ) { // table doesn't already exist
+
+		$query = "CREATE TABLE IF NOT EXISTS " . $wpdb->user2group_rs . ' (';
+		 
+		foreach ($cols as $colname => $typedef)
+			$query .= $colname . ' ' . $typedef . ',';
+		 
+		$query .= "PRIMARY KEY user2group ($wpdb->user2group_uid_col, $wpdb->user2group_gid_col));";
+		
+		$wpdb->query($query);
+		
+	} else {
+
+		// if existing table was found but specified groupid and userid columns are invalid, bail
+		$tablefields = $wpdb->get_col("DESC $wpdb->user2group_rs", 0);
+		
+		foreach ($tablefields as $column) if ($column == $wpdb->user2group_gid_col) break;	
+		if ($column != $wpdb->user2group_gid_col)
+			wp_die ( sprintf( 'Database config error: specified ID column (%1$s) not found in table %2$s', $wpdb->user2group_gid_col, $wpdb->user2group_rs ) );
+		
+		foreach ($tablefields as $column) if ($column == $wpdb->user2group_uid_col) break;	
+		if ($column != $wpdb->user2group_uid_col)
+			wp_die ( sprintf( 'Database config error: specified ID column (%1$s) not found in table %2$s', $wpdb->user2group_uid_col, $wpdb->user2group_rs ) );
+
+		foreach ($cols as $requiredcol_name => $requiredcol_typedef) {
+			foreach ($tablefields as $col_name)
+				if ($requiredcol_name == $col_name) break;
+				
+			if ($requiredcol_name != $col_name)
+				//column was not found, so create it
+				$wpdb->query("ALTER TABLE $wpdb->user2group_rs ADD COLUMN $requiredcol_name $requiredcol_typedef");
+		}	
+	}
+	
+	$tabledefs='';
+
+	/*  user2role2object_rs: 
+		
+		(scope == 'object' ) => for the specified object, all users in specified group have all caps in specified role
+					- OR -
+		(scope == 'term' ) => for all entities for which the specified object is a category, all users in specified group have all caps in specified role
+	
+		(assign_for = 'children' or 'both' ) => new children of the specified object inherit this object_role_name
+	
+	Abstract object type to support group control of new content-specific roles without revising db schema
+	
+	note: Term roles are retrieved and buffered into memory for the current user at login.
+	*/
+	
+	// note: dbDelta requires two spaces after PRIMARY KEY, no spaces between KEY columns
+	$tabledefs .= "CREATE TABLE $wpdb->user2role2object_rs (
+	 assignment_id bigint(20) unsigned NOT NULL auto_increment,
+	 user_id bigint(20) unsigned NULL,
+	 group_id bigint(20) unsigned NULL,
+	 role_name varchar(32) NOT NULL default '',
+	 role_type enum('rs', 'wp', 'wp_cap') NOT NULL default 'rs',
+	 scope enum('blog', 'term', 'object') NOT NULL,
+	 src_or_tx_name varchar(32) NOT NULL default '',
+	 obj_or_term_id bigint(20) unsigned NOT NULL default '0',
+	 assign_for enum('entity', 'children', 'both') NOT NULL default 'entity',
+	 inherited_from bigint(20) unsigned NOT NULL default '0',
+	 assigner_id bigint(20) unsigned NOT NULL default '0',
+	 	PRIMARY KEY  (assignment_id),
+	 	KEY role2obj (scope,assign_for,role_type,role_name,src_or_tx_name,obj_or_term_id),
+	 	KEY role2agent (assign_for,scope,role_type,role_name,group_id,user_id),
+	 	KEY role (role_type,role_name,scope,assign_for,src_or_tx_name,group_id,user_id,obj_or_term_id),
+	 	KEY role_assignments (role_name,role_type,scope,assign_for,src_or_tx_name,group_id,user_id,obj_or_term_id,inherited_from,assignment_id)
+	);
+	";
+	
+	$tabledefs .= "CREATE TABLE $wpdb->role_scope_rs (
+	 requirement_id bigint(20) NOT NULL auto_increment,
+	 role_name varchar(32) NOT NULL default '',
+	 role_type enum('rs', 'wp', 'wp_cap') NOT NULL default 'rs',
+	 topic enum('blog', 'term', 'object') NOT NULL,
+	 src_or_tx_name varchar(32) NOT NULL default '',
+	 obj_or_term_id bigint(20) NOT NULL default '0',
+	 max_scope enum('blog', 'term', 'object') NOT NULL,
+	 require_for enum('entity', 'children', 'both') NOT NULL default 'entity',
+	 inherited_from bigint(20) NOT NULL default '0',
+	 	PRIMARY KEY  (requirement_id),
+	 	KEY role_scope (max_scope,topic,require_for,role_type,role_name,src_or_tx_name,obj_or_term_id),
+	 	KEY role_scope_assignments (max_scope,topic,require_for,role_type,role_name,src_or_tx_name,obj_or_term_id,inherited_from,requirement_id)
+	);
+	";
+	
+	// apply all table definitions
+	dbDelta($tabledefs);
+	
+} //end update_schema function
+
+
+function scoper_update_supplemental_schema($table_name) {
+	global $wpdb;
+
+	require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+
+	if ( 'data_rs' == $table_name ) {
+		$tabledefs .= "CREATE TABLE {$wpdb->prefix}{$table_name} (
+		 rs_id bigint(20) NOT NULL auto_increment,
+		 topic enum('term', 'object') default 'object',
+		 src_or_tx_name varchar(32) NOT NULL default '',
+		 object_type varchar(32) NOT NULL default '',
+		 actual_id bigint(20) NOT NULL default '0',
+		 name text NOT NULL default '',
+		 parent bigint(20) NOT NULL default '0',
+		 owner bigint(20) NOT NULL default '0',
+		 status varchar(20) NOT NULL default '',
+		 	PRIMARY KEY  (rs_id),
+		 	KEY actual_id (actual_id,src_or_tx_name,object_type,topic)
+		);
+		";
+		
+		// apply all table definitions
+		dbDelta($tabledefs);
+		
+		return true;
+	}
+}
+?>
---
+++ hardway/hardway-admin_rs.php
@@ -0,0 +1,356 @@
+<?php
+
+if( basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME']) )
+	die();
+
+if ( ! awp_ver('2.7-dev') )
+	require_once('hardway-admin-legacy_rs.php');
+	
+global $scoper;
+
+// WP 2.5 - 2.7 autosave wipes out page parent. WP 2.5 autosave sets author to current user
+if ( isset($_POST['action']) && ($_POST['action'] == 'autosave') && isset($_POST['post_type']) )
+	add_filter('query', array('ScoperAdminHardway', 'flt_autosave_bugstomper') );
+
+if ( is_admin() ) {
+	$nomess_uris = apply_filters( 'scoper_skip_lastresort_filter_uris', array( 'p-admin/categories.php', 'p-admin/themes.php', 'p-admin/plugins.php', 'p-admin/profile.php' ) );
+
+	// filter users list for edit-capable users as a convenience to administrator
+	// URIs ending in specified filename will not be subjected to low-level query filtering
+	if ( ! agp_string_ends_in($_SERVER['REQUEST_URI'], $nomess_uris ) )
+		add_filter('query', array('ScoperAdminHardway', 'flt_editable_user_ids') );
+	
+	// wrapper to users_where filter for "post author" / "page author" dropdown (limit to users who have appropriate caps)
+  	add_filter('get_editable_authors', array('ScoperAdminHardway', 'flt_get_editable_authors'), 50, 1);
+ 
+  	add_filter('wp_dropdown_users', array('ScoperAdminHardway', 'flt_wp_dropdown_users'), 50, 1);	
+  	
+	// TODO: only for edit / write post/page URIs and dashboard ?
+	if ( awp_ver('2.6') )
+		add_filter('query', array('ScoperAdminHardway', 'flt_include_pending_revisions') );
+}
+
+if ( ! $is_administrator = is_administrator_rs() ) {
+	require_once( 'hardway-admin_non-administrator_rs.php' );
+}
+
+if ( (is_admin() || defined('XMLRPC_REQUEST') ) && ! $is_administrator ) {
+	// URIs ending in specified filename will not be subjected to low-level query filtering
+	$nomess_uris = array_merge($nomess_uris, array('p-admin/admin-ajax.php'));
+
+	if ( ! agp_string_ends_in($_SERVER['REQUEST_URI'], $nomess_uris, true ) ) {  //arg: actually return true for any strpos value
+		add_filter('query', array('ScoperAdminHardway_Ltd', 'flt_last_resort_query') );
+	}
+}
+
+if ( is_admin() && ! $is_administrator ) {
+	// limit these links on post/page edit listing to drafts which current user can edit
+	add_filter('get_others_drafts', array('ScoperAdminHardway_Ltd', 'flt_get_others_drafts'), 50, 1);
+} // endif filtering disabled for admin access type
+
+
+/**
+ * ScoperAdminHardway PHP class for the WordPress plugin Role Scoper
+ * hardway-admin_rs.php
+ * 
+ * @author 		Kevin Behrens
+ * @copyright 	Copyright 2009
+ * 
+ * Used by Role Role Scoper Plugin as a container for statically-called functions
+ *
+ */
+class ScoperAdminHardway {
+	// WP autosave wipes out page parent and sets author to current user
+	function flt_autosave_bugstomper($query) {
+		global $wpdb;
+
+		if ( ( strpos($query, "PDATE $wpdb->posts ") && strpos($query, "post_parent") ) ) {
+			// as of WP 2.6, only the post_parent is being wiped.
+			if ( ! awp_ver('2.6') ) {
+				global $current_user;
+				$query = preg_replace( "/,\s*`post_author`\s*=\s*'{$current_user->ID}'/", "", $query);
+				$query = preg_replace( "/`post_author`\s*=\s*'{$current_user->ID}',/", "", $query);
+			}
+			
+			$query = preg_replace( "/,\s*`post_parent`\s*=\s*'0'/", "", $query);
+		}
+
+		return $query;
+	}
+	
+	function flt_include_pending_revisions($query) {
+		// no recursion
+		if ( scoper_querying_db() )
+			return $query;
+		
+		// Require current user to be a blog-wide editor due to complexity of applying scoped roles to revisions
+		if ( strpos($query, ".post_status = 'pending'") || strpos($query, ".post_status = 'inherit'") || strpos($query, 'GROUP BY post_status') ) {
+		
+			// Not yet able to support pending revision restoration by non-administrator
+			/*
+			global $scoper, $current_user;
+			
+			$page_roles = $scoper->role_defs->qualify_roles('edit_others_pages');
+			$post_roles = $scoper->role_defs->qualify_roles('edit_others_posts');
+			
+			$is_blogwide_editor = array();
+			$is_blogwide_editor['post'] = array_intersect_key($post_roles, $current_user->blog_roles);
+			$is_blogwide_editor['page'] = array_intersect_key($page_roles, $current_user->blog_roles);
+			
+			if ( $is_blogwide_editor['post'] || $is_blogwide_editor['page'] ) {
+			*/
+			if ( is_administrator_rs() ) {
+				global $wpdb;
+				
+				//$object_type = ( strpos($query, 'page') ) ? 'page' : 'post';
+				
+				//if ( $is_blogwide_editor[$object_type] ) {
+					if ( strpos($query, ".post_status = 'pending'") && strpos($query, 'ELECT') && ! strpos($query, ".post_status = 'draft'") ) {
+						if ( awp_ver('2.7-dev') ) {
+							$query = str_replace("$wpdb->posts.post_type = 'page'", "$wpdb->posts.post_type IN ('page', 'revision') AND ($wpdb->posts.post_status = 'pending')", $query);
+							$query = str_replace("$wpdb->posts.post_type = 'post'", "$wpdb->posts.post_type IN ('post', 'revision') AND ($wpdb->posts.post_status = 'pending')", $query);
+						} else {
+							$query = str_replace("$wpdb->posts.post_type = 'page' AND ($wpdb->posts.post_status = 'pending')", "$wpdb->posts.post_type IN ('page', 'revision') AND ($wpdb->posts.post_status = 'pending')", $query);
+							$query = str_replace("$wpdb->posts.post_type = 'post' AND ($wpdb->posts.post_status = 'pending')", "$wpdb->posts.post_type IN ('post', 'revision') AND ($wpdb->posts.post_status = 'pending')", $query);
+						}
+					}
+				
+					if ( strpos($query, "GROUP BY post_status") )
+						$query = str_replace("SELECT post_status, COUNT( * ) AS num_posts FROM $wpdb->posts WHERE post_type = 'page'", "SELECT post_status, COUNT( * ) AS num_posts FROM $wpdb->posts WHERE ( post_type = 'page' OR ( post_type = 'revision' AND post_status = 'pending' ) )", $query);
+				//}
+				
+				if ( strpos($query, "inherit") && strpos($query, "ELECT") )
+					$query = str_replace("post_type = 'revision' AND ($wpdb->posts.post_status = 'inherit')", "post_type = 'revision' AND ($wpdb->posts.post_status IN ('inherit', 'pending') )", $query);
+
+			} // endif current user is allowed to "restore" pending revisions
+		} // endif query pertains in any way to pending status and/or revisions
+		
+		return $query;
+	}
+
+	// Filter the otherwise unfilterable get_editable_user_ids() result set, which affects the admin UI
+	function flt_editable_user_ids($query) {
+		// Only display users who can read / edit the object in question
+		if ( strpos ($query, "user_id FROM") && strpos ($query, "meta_key =") ) {
+			global $wpdb, $scoper;
+			
+			if ( strpos ($query, "user_id FROM $wpdb->usermeta WHERE meta_key = '{$wpdb->prefix}user_level'") ) {
+				if ( isset( $_POST['post_type']) ) {
+					if ( 'page' == $_POST['post_type'] )
+						$reqd_caps = array('edit_pages');
+					else
+						$reqd_caps = array('edit_posts');
+				
+					if ( isset($_POST['post_ID']) )
+						$object_id = $_POST['post_ID'];
+						
+				} elseif ( $context = $scoper->admin->get_context('') ) {//arg: return reqd_caps only {
+					if ( $context->source ) {
+						$src_name = $context->source->name;
+						$object_id = $scoper->data_sources->detect('id', $src_name);
+					} else {
+						$src_name = '';
+						$object_id = 0;
+					}
+					
+					$reqd_caps = $context->reqd_caps;
+				}
+				
+				if ( ! empty($reqd_caps) ) {
+					global $scoper, $current_user;
+
+					if ( 'page' == $context->object_type_def->val )
+						$current_user_reqd_cap = 'edit_others_pages';
+					else
+						$current_user_reqd_cap = 'edit_others_posts';
+					
+					$id = $scoper->data_sources->detect('id', 'post');
+
+					// only modify the default authors list if current user can edit_others for the current post/page
+					if ( current_user_can( $current_user_reqd_cap, $id ) ) {
+						$users = $scoper->users_who_can($reqd_caps, COL_ID_RS, $src_name, $object_id);
+	
+						if ( ! in_array($current_user->ID, $users) )
+							$users []= $current_user->ID;
+						
+						$query = "SELECT $wpdb->users.ID FROM $wpdb->users WHERE ID IN ('" . implode("','", $users) . "')";
+					}
+				}
+			}
+		}
+		
+		return $query;
+	}
+	
+	//horrible reverse engineering of dropdown_users execution because only available filter is on html output
+	function flt_wp_dropdown_users($wp_output) {
+		// if (even after our blogcap tinkering) the author list is already locked due to insufficient blog-wide caps, don't mess
+		if ( ! $pos = strpos ($wp_output, '<option') )
+			return $wp_output;
+		
+		if ( ! strpos ($wp_output, '<option', $pos + 1) )
+			return $wp_output;
+
+		global $wpdb, $scoper;
+		
+		// This uri-checking functions will be called by flt_users_where.
+		// If the current uri is not recognized, don't bother with the painful parsing. 
+		$context = $scoper->admin->get_context('');
+	
+		if ( empty ($context->reqd_caps) )
+			return $wp_output;
+			
+		if ( 'page' == $context->object_type_def->val )
+			$current_user_reqd_cap = 'edit_others_pages';
+		else
+			$current_user_reqd_cap = 'edit_others_posts';
+		
+		$id = $scoper->data_sources->detect('id', 'post');
+
+		// only modify the default authors list if current user can edit_others for the current post/page
+		if ( ! current_user_can( $current_user_reqd_cap, $id ) )
+			return $wp_output;
+			
+		$src_name = $context->source->name;
+			
+		$last_query = $wpdb->last_query;
+		$orderpos = strpos($last_query, 'ORDER BY');
+		$orderby = ( $orderpos ) ? substr($last_query, $orderpos) : '';
+		$id_in = $id_not_in = $show_option_all = $show_option_none = '';
+		
+		$pos = strpos($last_query, 'AND ID IN(');
+		if ( $pos ) {
+			$pos_close = strpos($last_query, ')', $pos);
+			if ( $pos_close)
+				$id_in = substr($last_query, $pos, $pos_close - $pos + 1); 
+		}
+		
+		$pos = strpos($last_query, 'AND ID NOT IN(');
+		if ( $pos ) {
+			$pos_close = strpos($last_query, ')', $pos);
+			if ( $pos_close)
+				$id_not_in = substr($last_query, $pos, $pos_close - $pos + 1); 
+		}
+		
+		$search = "<option value='0'>";
+		$pos = strpos($wp_output, $search . __('Any'));
+		if ( $pos ) {
+			$pos_close = strpos($wp_output, '</option>', $pos);
+			if ( $pos_close)
+				$show_option_all = substr($wp_output, $pos + strlen($search), $pos_close - $pos - strlen($search)); 
+		}
+		
+		$search = "<option value='-1'>";
+		$pos = strpos($wp_output, $search . __('None'));
+		if ( $pos ) {
+			$pos_close = strpos($wp_output, '</option>', $pos);
+			if ( $pos_close)
+				$show_option_none = substr($wp_output, $pos + strlen($search), $pos_close - $pos - strlen($search)); 
+		}
+		
+		$search = "<select name='";
+		$pos = strpos($wp_output, $search);
+		if ( false !== $pos ) {
+			$pos_close = strpos($wp_output, "'", $pos + strlen($search));
+			if ( $pos_close)
+				$name = substr($wp_output, $pos + strlen($search), $pos_close - $pos - strlen($search)); 
+		}
+		
+		$search = " id='";
+		$multi = ! strpos($wp_output, $search);  // beginning with WP 2.7, some users dropdowns lack id attribute
+		
+		$search = " class='";
+		$pos = strpos($wp_output, $search);
+		if ( $pos ) {
+			$pos_close = strpos($wp_output, "'", $pos + strlen($search));
+			if ( $pos_close)
+				$class = substr($wp_output, $pos + strlen($search), $pos_close - $pos - strlen($search)); 
+		}
+		
+		$search = " selected='selected'";
+		$pos = strpos($wp_output, $search);
+		if ( $pos ) {
+			$search = "<option value='";
+	
+			$str_left = substr($wp_output, 0, $pos);
+			$pos = strrpos($str_left, $search); //back up to previous option tag
+
+			$pos_close = strpos($wp_output, "'", $pos + strlen($search));
+			if ( $pos_close)
+				$selected = substr($wp_output, $pos + strlen($search), $pos_close - ($pos + strlen($search)) ); 
+		}
+		
+		// Role Scoper filter application
+		$where = "$id_in $id_not_in";
+		
+		$object_id = 0;
+		$reqd_caps = array();
+		if ( $context = $scoper->admin->get_context() ) {
+			if ( ! empty($context->source) )
+				$object_id = $scoper->data_sources->detect('id', $context->source);
+			
+			if ( isset($context->reqd_caps) )
+				$reqd_caps = $context->reqd_caps;
+		}
+		
+		$args = array();
+		$args['where'] = $where;
+		$args['orderby'] = $orderby;
+		
+		if ( $object_id ) {
+			if ( $current_author = $scoper->data_sources->get_from_db('owner', $src_name, $object_id) )
+				$args['preserve_or_clause'] = " uro.user_id = '$current_author'";
+		} else {
+			global $current_user;
+			$args['preserve_or_clause'] = " uro.user_id = '$current_user->ID'";
+		}
+		
+		$users = $scoper->users_who_can($reqd_caps, COLS_ID_DISPLAYNAME_RS, $src_name, $object_id, $args);
+
+		$show = 'display_name'; // no way to back this out
+
+		// ----------- begin wp_dropdown_users code copy (from WP 2.7) -------------
+		$id = $multi ? "" : "id='$name'";
+
+		$output = "<select name='$name' $id class='$class'>\n";
+
+		if ( $show_option_all )
+			$output .= "\t<option value='0'>$show_option_all</option>\n";
+
+		if ( $show_option_none )
+			$output .= "\t<option value='-1'>$show_option_none</option>\n";
+			
+		foreach ( (array) $users as $user ) {
+			$user->ID = (int) $user->ID;
+			$_selected = $user->ID == $selected ? " selected='selected'" : '';
+			$display = !empty($user->$show) ? $user->$show : '('. $user->user_login . ')';
+			$output .= "\t<option value='$user->ID'$_selected>" . wp_specialchars($display) . "</option>\n";
+		}
+
+		$output .= "</select>";
+		// ----------- end wp_dropdown_users code copy (from WP 2.7) -------------
+		
+		return $output;
+	}
+	
+	function flt_get_editable_authors($unfiltered_results) {
+		global $wpdb, $scoper;
+		
+		$context = $scoper->admin->get_context('', true); // arg: return reqd_caps only
+		
+		if ( ! empty ($context->reqd_caps) ) {
+			if ( 'page' == $matched->object_type_def->val )
+				$current_user_reqd_cap = 'edit_others_pages';
+			else
+				$current_user_reqd_cap = 'edit_others_posts';
+			
+			$id = $scoper->data_sources->detect('id', 'post');
+			
+			if ( current_user_can( $current_user_reqd_cap, $id ) )
+				return $scoper->users_who_can($context->reqd_caps, COLS_ALL_RS);
+		}
+
+		return $unfiltered_results;
+	}
+}
+?>
