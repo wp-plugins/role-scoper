32a6cf093ec1dd28dcd5cb866bd245de97867563
kevinB <kevinB@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-12 00:43:06 +0000

    v 1.2.9.RC2 and 1.3.RC2
    * BugFix : Fatal Error when editing a post with a Contributor and a non-default blog-wide role requirement is specified in Roles > Options > Advanced > Limited Editing Elements
    
    * Compat : When Revisionary plugin is also active, Contributor / Revisors can publish posts directly without review 
    
    * Compat : Pending Revisions (from Revisionary plugin) were not included in Edit Posts listing for non-Administrators when editing access to published post is affected by category-specific roles or restrictions (since 1.0)
    
    * BugFix : PHP Warning for "element is not an array" 
    
    git-svn-id: https://plugins.svn.wordpress.org/role-scoper/tags/1.2.9.RC2@299144 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ revisionary-helper_rs.php
@@ -0,0 +1,52 @@
+<?php
+
+Class Rvy_Helper {
+	
+	// Allow contributors and revisors to edit published post/page, with change stored as a revision pending review
+	function convert_post_edit_caps( $rs_reqd_caps, $post_type )	{
+		global $revisionary, $scoper;
+				
+		if ( ! empty( $revisionary->skip_revision_allowance ) || ! rvy_get_option('pending_revisions') )
+			return $rs_reqd_caps;
+		
+		$post_id = $scoper->data_sources->detect('id', 'post');
+
+		$status = get_post_field( 'post_status', $post_id, 'post' );
+
+		if ( awp_ver( '3.0' ) ) {
+			// don't need to fudge the capreq for post.php unless existing post has public/private status
+			$status_obj = get_post_status_object( $status );
+			
+			if ( empty( $status_obj->public ) && empty( $status_obj->private ) && ( 'future' != $status ) ) 
+				return $rs_reqd_caps;
+				
+			if ( $type_obj = get_post_type_object( $post_type ) ) {
+				$replace_caps = array( 'edit_published_posts', 'edit_private_posts', 'publish_posts', $type_obj->cap->edit_published_posts, $type_obj->cap->edit_private_posts, $type_obj->cap->publish_posts );
+				$use_cap_req = $type_obj->cap->edit_posts;
+			} else
+				$replace_caps = array();		
+		} else {
+			if ( ! in_array( $status, array( 'publish', 'private', 'future' ) ) )
+				return $rs_reqd_caps;
+		
+			// if ( strpos($script_name, 'p-admin/page.php') || strpos($script_name, 'p-admin/edit-pages.php') || ( ! empty( $_GET['post_type'] ) && 'page' == $_GET['post_type'] ) )
+			if ( 'page' == $post_type ) {
+				$replace_caps = array('edit_published_pages', 'edit_private_pages', 'publish_pages');
+				$use_cap_req = 'edit_pages';
+			} else {
+				$replace_caps = array( 'edit_published_posts', 'edit_private_posts', 'publish_posts' );
+				$use_cap_req = 'edit_posts';
+			}	
+		}
+
+		if ( array_intersect( $rs_reqd_caps, $replace_caps) ) {	
+			foreach ( $rs_reqd_caps as $key => $cap_name )
+				if ( in_array($cap_name, $replace_caps) )
+					$rs_reqd_caps[$key] = $use_cap_req;
+		}
+
+		return $rs_reqd_caps;
+	}
+}
+
+?>
